f49909fa76c2d7262cdacbfde5eb0561ded7fdb7
PrinceSP <princepasombaran@gmail.com>
2021-05-08 22:31:50 +0800

    starting making the get started screen

---
+++ src/pages/splash/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text,StyleSheet} from 'react-native'
+import {Button} from '../../components'
+
+const Splash = ()=>{
+  return(
+    <View>
+      <Text>lkdfjslkdj</Text>
+    </View>
+  )
+}
+
+export default Splash
