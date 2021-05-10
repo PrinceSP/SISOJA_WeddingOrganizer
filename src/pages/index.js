d0485c1f6764705593ceeaf9ae051f21d324037a
gabrielposumah <gjposumah@gmail.com>
2021-05-10 14:43:46 +0800

    add login integration firebase and direct

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import GetStarted from './getStarted'
 import Splash from './splash'
 import Login from './login'
 import SignUp from './signUp'
+import Discover from './discover'
 
-export {Splash,GetStarted,Login,SignUp}
+export {Splash,GetStarted,Login,SignUp,Discover}
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {ScrollView,View,StyleSheet,Text} from 'react-native'
 import {LoginBG} from '../../assets'
 import {LoginMol,Buttons,Footer,WeddingTitle} from '../../components'
+import firebase from '../../config/firebase'
+import {showMessage} from 'react-native-flash-message'
 
 const Login = ({navigation}) => {
+  const [email,setEmail] = useState('')
+  const [password,setPass] = useState('')
+
+  const submit=()=>{
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(res=>{
+      navigation.replace('Discover')
+      setEmail('')
+      setPass('')
+    })
+  }
+
   return (
-    <View style={style.container}>
-      <LoginBG/>
-      <WeddingTitle/>
-      <LoginMol/>
-      <Text style={{fontSize:19,position:'absolute',bottom:245,color:'#444'}}>OR</Text>
-      <Buttons position='absolute' top={500} width={362} title='Sign Up' bgColor='#FF8527' color='#fff' radius={8} height={54} onPress={()=>navigation.navigate('SignUp')}/>
-      <Footer/>
-    </View>
+    <ScrollView>
+      <View style={style.container}>
+        <LoginBG style={{flex:1}}/>
+        <WeddingTitle/>
+        <LoginMol
+          valEmail={email}
+          valPass={password}
+          changeEmail={valEmail=>setEmail(valEmail)}
+          changePass={valPass=>setPass(valPass)}
+          onPress={submit}
+          />
+        <Text style={style.or}>OR</Text>
+        <Buttons position='absolute' top={500} width={362} title='Sign Up' bgColor='#FF8527' color='#fff' radius={8} height={54} onPress={()=>navigation.navigate('SignUp')}/>
+        <Footer/>
+      </View>
+    </ScrollView>
   )
 }
 
@@ -23,5 +45,11 @@ const style = StyleSheet.create({
     alignItems:'center',
     flex:1,
     flexDirection:'column',
+  },
+  or:{
+    fontSize:19,
+    position:'absolute',
+    bottom:245,
+    color:'#444'
   }
 })
